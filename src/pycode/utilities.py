9eb03e051737c9c6dd5ed4b0d2a83fd08409ace8
nannanpeng <npeng@chapman.edu>
2021-03-05 20:42:23 -0800

    add jit

--- src/pycode/utilities.py
+++ src/pycode/utilities.py
@@ -1,28 +1,35 @@
 # This script includes all the preference functions,
 # like utility, human capital accumulation and so on.
+import numpy as np
+from numba import njit
 
-# jitable
+
+@njit
 def ConUtility(c, ChiC, iota):
-    con_utility = ChiC*c**(1-iota)/(1-iota)
-    return con_utility
+    if iota == 1:
+        con_utility = ChiC*np.log(c)
+        return con_utility
+    else:
+        con_utility = ChiC*c**(1-iota)/(1-iota)
+        return con_utility
 
 
-# jitable
+@njit
 def LabUtility(labor, ChiL, psi):
     labor_utility = ChiL*labor**(1+psi)/(1+psi)
     return labor_utility
 
 
-# jitable
-def TotUtility(c, labor, *args):
+@njit
+def TotUtility(c, labor, args):
     ChiC, iota, ChiL, psi = args
     con_utility = ConUtility(c, ChiC, iota)
     lab_utility = LabUtility(labor, ChiL, psi)
     return con_utility - lab_utility
 
 
-# jitable
-def HumanCapitalAccu(h, labor, *args):
+@njit
+def HumanCapitalAccu(h, labor, args):
     A, GammaL, GammaH, Xi = args
     accu = A*labor**GammaL*h**GammaH
     remaining = (1 - Xi)*h
